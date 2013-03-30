f5620aa5c02ea9756a2b62b1eeed170bc8fe0ad7
Sean Lee <sean@weaveus.com>
2013-03-30 12:10:09 +0900

    build-deps

---
+++ build-deps.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+RELEASE=`lsb_release -sr`
+INSTALLS="git-core
+pkg-config
+make
+autoconf 
+libtool 
+g++ 
+tcl 
+unzip 
+libyajl-dev 
+libyajl1 
+qt4-qmake
+libsqlite3-dev 
+curl 
+gperf 
+bison 
+libglib2.0-dev 
+libssl-dev 
+libxi-dev 
+libxrandr-dev 
+libxfixes-dev
+libxcursor-dev
+libfreetype6-dev
+libxinerama-dev
+libgl1-mesa-dev
+libgstreamer0.10-dev
+libgstreamer-plugins-base0.10-dev
+flex
+libicu-dev
+libboost-system-dev
+libboost-filesystem-dev
+libboost-regex-dev
+libboost-program-options-dev
+liburiparser-dev 
+libc-ares-dev
+libsigc++-2.0-dev
+libglibmm-2.4-dev
+libdb4.8-dev
+libcurl4-openssl-dev 
+xcb
+libx11-xcb-dev
+libxcb-sync0-dev
+libxcb1-dev
+libxcb-keysyms1-dev
+libxcb-image0-dev
+libxcb-render-util0-dev"
+
+if [ "$RELEASE" = "12.04" ] ; then
+  INSTALLS="$INSTALLS libxcb-icccm4-dev"
+else
+  INSTALLS="$INSTALLS libxcb-icccm1-dev"
+fi
+
+echo $INSTALLS
+
+echo "to build:"
+echo " $ ./build-webos-desktop.sh"
